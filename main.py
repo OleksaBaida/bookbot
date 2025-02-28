2590fc5349d4f7bc25e057da1e12dc9df2b6e598
OleksaBaida <8527729+OleksaBaida@users.noreply.github.com>
2025-02-28 21:20:23 +0200

    created count_words function

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 def main():
-    print(get_book_text('books/frankenstein.txt'))
+    path_to_book = 'books/frankenstein.txt'
+    string = get_book_text('books/frankenstein.txt')
+    num_words = count_words(string)
+    print(f"{num_words} words found in the document")
 
 def get_book_text(path_to_file):
     text = ''
@@ -7,4 +10,10 @@ def get_book_text(path_to_file):
         text = book.read()
     return text
 
+def count_words(string_to_count):
+    count = len(string_to_count.split())
+    return count
+
 main()
+# # string =
+# print(len(get_book_text('books/frankenstein.txt').split()))
